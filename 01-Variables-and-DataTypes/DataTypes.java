0917617c104cb904d6b12a273bdae51d505d458e
Shruti Srivastava <06srivastavashruti@gmail.com>
2026-07-25 03:08:54 +0530

    Create DataTypes.java

---
+++ 01-Variables-and-DataTypes/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 200;
+        int c = 5000;
+        long d = 100000L;
+
+        float e = 5.6f;
+        double f = 10.234;
+
+        char g = 'J';
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
