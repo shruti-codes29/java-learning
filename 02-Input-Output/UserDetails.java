58150b6347498c950375c51edda7e4cdb5a8ecb7
Shruti Srivastava <06srivastavashruti@gmail.com>
2026-07-25 03:34:52 +0530

    Create UserDetails.java

---
+++ 02-Input-Output/UserDetails.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class UserDetails {
+     public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter user's name:");
+
+       String name = sc.nextLine();
+       System.out.println("Enter user's age:");
+       int age = sc.nextInt();
+
+       System.out.println("Enter user's city:");
+       String city = sc.nextLine();\
+     }
+}
