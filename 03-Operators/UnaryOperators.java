0b90c3e86422dc50794687af083aa181465227f1
Shruti Srivastava <06srivastavashruti@gmail.com>
2026-08-02 01:29:46 +0530

    Create UnaryOperators.java

---
+++ 03-Operators/UnaryOperators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class UnaryOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int x = sc.nextInt();
+
+        System.out.println("x  :"+x);
+        System.out.println("+x  :"+(+x));
+        System.out.println("-x  :"+(-x));
+        System.out.println("++x  :"+(++x));
+        System.out.println("x++  :"+(x++));
+        System.out.println("--x  :"+(--x));
+        System.out.println("x--  :"+(x--));
+    }
+}
